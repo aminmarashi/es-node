69c1ee48698b2e8bd9a4e3d49b07ba431c6da495
Amin <amin@lit.codes>
2021-11-13 04:21:16 +0100

    Initial files

---
+++ index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import { exec, spawnSync } from 'child_process';
+import { existsSync } from 'fs';
+
+const esbuildOptions = process.env.ESBUILD_OPTIONS || '--bundle --platform=node'
+const args = process.argv.slice(2);
+let filename;
+
+const nodeArgs = args.filter(arg => {
+  if (arg[0] !== '-') {
+    debugger;
+    if (filename) {
+      throw Error(`Invalid options, only one option cannot start with a '-', options: ${args.join(', ')}`)
+    } else {
+      filename = arg;
+    }
+  }
+});
+
+if (!existsSync(filename)) {
+  throw Error(`Cannot find the file to run: ${filename}`);
+}
+
+exec(`esbuild ${esbuildOptions} ${filename}`, (error, builtCode) => {
+  if (error) {
+    console.error('Cannot run esbuild,', error);
+    if (error.code === 127) {
+      console.error('Please make sure esbuild is installed globally: npm i -g esbuild')
+    }
+    return;
+  }
+  const { stdout, stderr } = spawnSync('node', nodeArgs, { input: builtCode });
+  process.stdout.write(stdout.toString());
+  process.stderr.write(stderr.toString());
+});
