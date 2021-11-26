01e684dfe85dc7c9405e0168f725e82cde02fe5f
amin <amin@fashion.cloud>
2021-11-26 13:52:28 +0100

    Add maxBuffer with 20MB default

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import { exec, spawnSync } from 'child_process';
 import { existsSync } from 'fs';
 
 const esbuildOptions = process.env.ESBUILD_OPTIONS || '--bundle --platform=node'
+const maxBuffer = process.env.JS_MAX_BUFFER || 1024^2 * 20; // 20 MB
 const args = process.argv.slice(2);
 let filename;
+
 
 const nodeArgs = args.filter(arg => {
   if (arg[0] !== '-') {
@@ -25,7 +27,7 @@ if (!existsSync(filename)) {
   throw Error(`Cannot find the file to run: ${filename}`);
 }
 
-exec(`esbuild ${esbuildOptions} ${filename}`, (error, builtCode) => {
+exec(`esbuild ${esbuildOptions} ${filename}`, { maxBuffer: Number(maxBuffer) }, (error, builtCode) => {
   if (error) {
     console.error('Cannot run esbuild,', error);
     if (error.code === 127) {
